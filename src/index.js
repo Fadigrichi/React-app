96aa2465b8368949974f1d29ee19ff12ab21cbed
fedi <grifedi0@gmail.com>
2022-10-09 12:06:44 +0100

    tchanges 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import All from './navbar/about'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <div>
-  <All />
+  <p>fedi grichi </p>
     </div>
 )
 
