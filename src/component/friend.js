67f43a344f094cd6763b929716fc1fc47b2a97e6
Fadigrichi <112814449+Fadigrichi@users.noreply.github.com>
2022-10-29 22:53:38 +0100

    Update friend.js

--- src/component/friend.js
+++ src/component/friend.js
@@ -1,11 +1,25 @@
- import React ,{useState} from "react";
+ import React from "react";
 
-export const friend =()=>{
-     let friends = useState();
+export const friend =(props)=>{
+    
 
   return(
    <div className="Friend">
+       <nav className="menu-button">
+        <p></p>
+        <p></p>
+        <p></p>
+       </nav>
 
+        <img src="" alt="image" className="image" />
+
+       <div className="c-l-lo-dis-p">
+         <p className="comment">comment</p>
+          <p className="like">like</p>
+          <p className="love">love</p>
+         <p className="dislike">dislike</p>
+       </div>
+     
     </div>
 )
 }
